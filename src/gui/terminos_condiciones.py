8e0362b0a63fae1a5926c4be06afacee284eacc5
Jordana Orfano <jordanaorfano@gmail.com>
2024-11-04 15:05:33 -0300

    Update terminos_condiciones.py

--- src/gui/terminos_condiciones.py
+++ src/gui/terminos_condiciones.py
@@ -13,13 +13,8 @@ class Terminos_CondicionesFrame(ctk.CTkFrame):
         self.terminosVentana.resizable(False, False) # La ventana no puede cambiar su tamaño
         self.terminosVentana.attributes("-topmost", "true") # Aparece adelante de la ventana actual
 
-        self.terminosLabel = ctk.CTkLabel(
-            self.terminosVentana, font=("", 24), text="Términos y condiciones"
-        )
-        self.terminosLabel.pack(pady=20, padx=0, fill="x")
-
         self.frame = ctk.CTkScrollableFrame(self.terminosVentana)
-        self.frame.pack(pady=0, padx=30, fill="both", expand=True)
+        self.frame.pack(pady=0, padx=0, fill="both", expand=True)
 
         self.terminosTxt = "\nBases y Condiciones del Software de EcoPrint.\n\nParticipación:\nEl software está disponible para la participación de interesados en compartir proyectos para ayudar al medio-ambiente e informar sobre eventos locales relacionados. La participación es voluntaria y abierta a personas mayores de 18 años.\n\nContenido:\nSe invita a los usuarios a subir proyectos relacionadas al tema, incluyendo eventos de interés comunitario.\n\nVeracidad de la Información:\nLos participantes deben proporcionar información veraz y corroborada en la medida de lo posible. La difusión de información falsa puede resultar en la exclusión del usuario del programa.\n\nRespeto y Ética:\nSe prohíbe la publicación de contenido difamatorio, ofensivo o que viole los derechos de privacidad de terceros. El respeto y la ética son fundamentales para mantener un ambiente colaborativo y seguro.\n\nConfidencialidad:\nLos usuarios deben ser conscientes de no compartir información que viole la privacidad de otras personas o comprometa la seguridad de la comunidad.\n\nUso Responsable:\nEl programa se proporciona con el propósito de mejorar el medio-ambiente y la conciencia comunitaria. Su uso debe ser responsable y respetuoso. Cualquier mal uso del software puede resultar en la desactivación de la cuenta del usuario.\n\nPropiedad Intelectual:\nAl subir contenido, los usuarios otorgan una licencia no exclusiva para su uso en la plataforma.\n\nColaboración Comunitaria:\nSe fomenta la colaboración y el intercambio constructivo de información entre los usuarios. La plataforma se reserva el derecho de moderar y eliminar contenido que viole las normas de colaboración.\n\nDuración:\nEl uso del software no tiene limitaciones de tiempo y estará sujeto a revisiones periódicas para garantizar su eficacia y cumplimiento de normas.\n"
 
